4978f45f43907a176676e270a204e7c8b0b9d2fe
Carlos Humberto Méndez Hernández <167920296+Blastkcat@users.noreply.github.com>
2025-11-23 21:11:53 -0600

    clon1

---
+++ DemoTemplateOGL/DemoTemplateOGL/Base/Scene.h
@@ -0,0 +1,135 @@
+#pragma once
+#include "Base/model.h"
+#include "SkyDome.h"
+#include "Terreno.h"
+#include "Billboard.h"
+#include "BillboardAnimation.h"
+#include "Billboard2D.h"
+#include "Texto.h"
+#include "Water.h"
+#include "Animator.h"
+#include "Animation.h"
+
+class Scene {
+	public:
+		virtual float getAngulo() = 0;
+		virtual void setAngulo(float angulo) = 0;
+		virtual Model* getMainModel() = 0;
+		virtual void setMainModel(Model* mainModel) = 0;
+		virtual Scene* Render() = 0;
+		virtual SkyDome* getSky() = 0;
+		virtual Terreno* getTerreno() = 0;
+		virtual std::vector<Model*> *getLoadedModels() = 0;
+		virtual std::vector<Billboard*> *getLoadedBillboards() = 0;
+		virtual std::vector<BillboardAnimation*> *getLoadedBillboardsAnimation() = 0;
+		virtual std::vector<Billboard2D*> *getLoadedBillboards2D() = 0;
+		virtual std::vector<Texto*> *getLoadedText() = 0;
+		virtual ~Scene(){
+		};
+
+		virtual int update(){
+            float angulo = getAngulo() + 1.5 * gameTime.deltaTime / 100;
+            angulo = angulo >= 360 ? angulo - 360.0 : angulo;
+            setAngulo(angulo);
+            getSky()->setRotY(angulo);
+			// Actualizar ciclo día/noche
+			getSky()->update(gameTime.deltaTime / 1000.0f);
+            Model* camara = getMainModel();
+			getTerreno()->lightPos = *camara->getNextTranslate();
+			for (int i = 0; i < getLoadedModels()->size(); i++){
+				auto it = getLoadedModels()->begin() + i;
+				Model *collider = NULL, *model = *it;
+				model->lightPos = *camara->getNextTranslate();
+				for (int j = 0; j < model->getModelAttributes()->size(); j++){
+					int idxCollider = -1;
+					bool objInMovement = (*model->getNextTranslate(j)) != (*model->getTranslate(j));
+					glm::vec3 &posM = objInMovement ? *model->getNextTranslate(j) : *model->getTranslate(j);
+					glm::vec3 ejeColision = glm::vec3(0);
+					bool isPrincipal = model == camara; // Si es personaje principal, activa gravedad
+					float terrainY = getTerreno()->Superficie(posM.x, posM.z);
+					if (model->name.compare("fogata") == 0 && j == 0) {
+						glm::vec3 positionA = *camara->getNextTranslate(); // extrae la traslación de A
+						glm::vec3 positionB = posM;
+						// Seguimiento al jugador
+						float followSpeed = 1.0f * gameTime.deltaTime / 1000;
+						positionB = glm::mix(positionB, positionA, followSpeed);
+						positionB.y =
+							getTerreno()->Superficie(positionB.x, positionB.z);
+						model->setNextTranslate(&positionB);
+					}
+					ModelCollider mcollider = model->update(terrainY, *getLoadedModels(), ejeColision, isPrincipal, j);
+					if (mcollider.model != NULL){
+						collider = (Model*)mcollider.model;
+						idxCollider = mcollider.attrIdx;
+					}
+					if (collider != NULL && model == camara){
+						if (ejeColision.y == 1){
+							INFO("APLASTADO!!!! " + collider->name, "JUMP HITBOX_"+to_string(idxCollider));
+							if (removeCollideModel(collider, idxCollider))
+								i--;
+						}
+					}
+					else if (collider != NULL) {
+						if (model->name.compare("fogata") == 0 && j == 0
+							&& collider == camara) {
+							bool agregar = true;
+							for (Texto* a : *getLoadedText()) {
+								if (a->name.compare("GAMEOVER") == 0) {
+									agregar = false;
+									break;
+								}
+							}
+							if (agregar) {
+								Texto* coordenadas = new Texto((WCHAR*)L"PERDISTE", 100, 0, 0, 44, 0, model);;
+								coordenadas->name = "GAMEOVER";
+								getLoadedText()->emplace_back(coordenadas);
+							}
+						}
+					}
+					if (j < 0) j = 0;
+				}
+				if (i < 0) i = 0;
+			}
+			// Actualizamos la camara
+            camara->cameraDetails->CamaraUpdate(camara->getRotY(), camara->getTranslate());
+            return -1;
+        }
+
+		virtual bool removeCollideModel(Model* collider, int idxCollider){
+			auto it = std::find(getLoadedModels()->begin(), getLoadedModels()->end(), collider);
+			if (idxCollider == 0){
+				collider->setActive(false);
+				Model* AABB = (Model*)collider->getModelAttributes()->at(0).hitbox;
+				if (AABB)
+					delete AABB;
+				collider->getModelAttributes()->at(0).hitbox = NULL;
+			} else {
+				ModelAttributes &attr = collider->getModelAttributes()->at(idxCollider);
+				Model *AABB = (Model*)attr.hitbox;
+				if (AABB)
+					delete AABB;
+				collider->getModelAttributes()->erase(collider->getModelAttributes()->begin() + idxCollider);
+			}
+			if (collider->getModelAttributes()->size() == 1 && !collider->getActive()){
+				it = std::find(getLoadedModels()->begin(), getLoadedModels()->end(), collider);
+				it = getLoadedModels()->erase(it);
+				delete collider;
+				return true;
+			}
+			return false;
+		}
+
+		virtual Model* lookForCollition(glm::vec3 &yPos, bool collitionMovement = false) {
+			std::vector<Model*> *ourModel = getLoadedModels();
+			Model *camara = getMainModel();
+			for (int i = 0; i < ourModel->size(); i++) {
+				Model *model = (*ourModel)[i];
+				if (model != camara) {
+					if (camara->colisionaCon(model->getModelAttributes()->at(0), yPos, collitionMovement)) {
+						return model;
+					}
+				}
+			}
+			return NULL;
+		}
+};
---
+++ DemoTemplateOGL/DemoTemplateOGL/shaders/models/1.model_material_loading.fs
@@ -0,0 +1,73 @@
+#version 330 core
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;    
+    float shininess;
+};
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    vec3 direction;
+};
+
+in vec3 FragPos;  
+in vec2 TexCoords;
+in vec3 Normal;  
+  
+out vec4 FragColor;
+
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
+uniform int textureSample = 1;
+uniform sampler2D texture_diffuse1;
+uniform vec4 color;
+
+void main()
+{    
+    
+
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightToFrag = normalize(FragPos - light.position);
+
+    // Difusa
+    float diff = max(dot(norm, lightToFrag), 0.0);
+    // specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightToFrag, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+   
+//    result *= color.rgb; // <--- Apply color tint here
+
+    float distance = length(FragPos - light.position);
+    float attenuation = 1.0 / (1.0 + 0.09 * distance + 0.032 * (distance * distance));
+
+    // Intensidad del foco
+    float theta = dot(lightToFrag, normalize(light.direction));  // sin el signo menos
+    float epsilon = cos(radians(12.5)) - cos(radians(5.5));
+    float intensity = clamp((theta - cos(radians(5.5))) / epsilon, 0.0, 1.0);
+
+    // Combinar
+    vec3 ambient  = light.ambient  * material.ambient;
+    vec3 diffuse  = light.diffuse  * diff * material.diffuse;
+    vec3 specular = light.specular * spec * material.specular;
+
+    ambient  *= attenuation;
+    diffuse  *= intensity * attenuation;
+    specular *= intensity * attenuation;
+
+    vec3 result = ambient + diffuse + specular;
+    if (textureSample == 1) {
+        vec4 texColor = texture(texture_diffuse1, TexCoords);
+        if (texColor.a < 0.1)
+            discard;
+        FragColor = vec4(result, 1.0) * texColor;
+    } else {
+        FragColor = vec4(result, 1.0); // use color alpha if you want
+    }
+}
